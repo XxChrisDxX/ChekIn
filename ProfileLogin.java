f86eec67b651681d9961cc942911403c481d6127
XxChrisDxX <skaterzrulecd@yahoo.com>
2016-12-06 03:53:06 -0500

    Add files via upload

--- ProfileLogin.java
+++ ProfileLogin.java
@@ -115,7 +115,7 @@ public class ProfileLogin extends javax.swing.JFrame {
     		String Email = emailcheck.readEmail(name);
     		if(password.equals(Email))
     			if(emailcheck.readPermissions(name)){
-    	        	new AdminProfile().setVisible(true);
+    	        	new AdminProfile(name).setVisible(true);
     	        	setVisible(false);
     	        }
     			else{
